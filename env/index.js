271acf32018e189504eab05da88b942d87520153
brighter <bostjan.rihter@gmail.com>
2023-02-01 17:59:15 +0100

    trigger build

--- env/index.js
+++ env/index.js
@@ -34,7 +34,7 @@ const aws = async (cfg) => {
   credentials = await getCredentials('aws')
   credentials = credentials[process.env.AWS_PROFILE]
 
-  cfg.aws.storageClient.credentials = {
+  cfg.s3.storageClient.credentials = {
     accessKeyId: credentials.aws_access_key_id,
     secretAccessKey: credentials.aws_secret_access_key
   }
