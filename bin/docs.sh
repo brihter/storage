26a86f09282f93ce4ea4821bd7458b43f2872dd0
brighter <bostjan.rihter@gmail.com>
2023-02-21 16:53:26 +0100

    wip

---
+++ bin/docs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+path_current=$(cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)
+path_root="$path_current/.."
+
+packages=("/src/storage" "/src/storage-adapter-s3")
+for path_package in "${packages[@]}"
+do
+  path="$path_root$path_package"
+  cd $path
+  npm run docs
+done
