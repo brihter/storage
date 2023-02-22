4980a768552a7abcc7952507263ab6ab401eb723
brighter <bostjan.rihter@gmail.com>
2023-02-22 17:59:08 +0100

    update dependency version

--- src/storage-adapter-s3/bin/version.sh
+++ src/storage-adapter-s3/bin/version.sh
@@ -4,6 +4,7 @@ version_next=$1
 
 path_current=$(cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)
 path_root="$path_current/.."
-
 cd $path_root
+
 cat <<< $(jq '.version="'"$version_next"'"' package.json) > package.json
+cat <<< $(jq '.dependencies."@brighter/storage"="^'"$version_next"'"' package.json) > package.json
