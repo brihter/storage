15ae0d4cd3c17c12871d94b713356b1ab4e8449e
brighter <bostjan.rihter@gmail.com>
2023-02-22 17:52:10 +0100

    wip

--- bin/version.sh
+++ bin/version.sh
@@ -5,7 +5,11 @@ version_next=$1
 path_current=$(cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)
 path_root="$path_current/.."
 
-packages=("/" "/src/storage" "/src/storage-adapter-s3")
+# self
+cat <<< $(jq '.version="'"$version_next"'"' package.json) > package.json
+
+# packages
+packages=("/src/storage" "/src/storage-adapter-s3")
 for path_package in "${packages[@]}"
 do
   path="$path_root$path_package"
