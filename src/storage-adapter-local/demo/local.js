bb100b9d15d38f7b1c85da2fc999e06aac286f08
brighter <bostjan.rihter@gmail.com>
2023-02-23 10:26:46 +0100

    wip

--- src/storage-adapter-local/demo/local.js
+++ src/storage-adapter-local/demo/local.js
@@ -1,11 +1,10 @@
 import { Storage } from '@brighter/storage-adapter-local'
 
-const config = {
+const storage = Storage({
   path: '/tmp/storage'
-}
+})
 
 const main = async () => {
-  const storage = Storage(config)
   await storage.write('msg', 'hi')
   const msg = await storage.read('msg')
   console.log(msg)
